eb0b6b3d53a6298cd6891c435cd8d0681659ec0d
MKujawa1 <maciej.kujawa96@o2.pl>
2022-06-21 22:05:24 +0200

    Get data, print results in loop with threading

--- main.py
+++ main.py
@@ -4,37 +4,37 @@ from random_user_agent.user_agent import UserAgent
 from random_user_agent.params import SoftwareName, OperatingSystem
 from time import sleep
 import random
-import matplotlib.pyplot as plt
-import numpy as np
 import threading
 
+### Generate random user agent
 software_names = [SoftwareName.CHROME.value]
 operating_systems = [OperatingSystem.WINDOWS.value,OperatingSystem.LINUX.value]
 user_agent_rotator = UserAgent(software_names=software_names,operating_systems =operating_systems,limit = 100)
-user_agent = user_agent_rotator.get_random_user_agent()
-global all_currents
-global all_times
-global all_bids
-global all_asks
-all_currents = []
-all_times = []
-all_bids = []
-all_asks = []
 
 def get_data():
+    '''
+    Get data from page. Print after scraping.
+
+    '''
+    ### Page to scraping
     URL = 'https://pl.investing.com/currencies/live-currency-cross-rates'
+    ### Init stop variable to break threading
+    global stop
+    stop = 0
+    ### i value to change state in while loop
+    i = 0
     while True:
+        ### Print names of columns
+        if i == 0:
+            print('\nCurrent time bid ask')
+        ### Get user agent and request
         user_agent = user_agent_rotator.get_random_user_agent()
         request = requests.get(URL,headers = {'User-Agent': user_agent})
-    
+        ### Get main data from page and tables
         cont = BeautifulSoup(request.content, 'html.parser')
-        
         tables = cont.find('div', class_ = 'js-cross-rates-table liveCurrencyBoxWrap')
         tables = tables.find_all('div',class_ = 'liveCurrencyBox')
-        currents = []
-        times = []
-        bids = []
-        asks = []
+        ### Get elements from each table
         for table in tables:
             current = table.find('div', class_ = 'topBox').a.text
             time = table.find('div', class_ = 'topBox').span.text
@@ -42,45 +42,35 @@ def get_data():
             bid_ask = bid_ask.find_all('div', class_ = 'innerContainerWrap')
             bid = float(bid_ask[0].text.split('\n')[2].replace('.','').replace(',','.'))
             ask = float(bid_ask[1].text.split('\n')[2].replace('.','').replace(',','.'))
-            
-            currents.append(current)
-            times.append(time)
-            bids.append(bid)
-            asks.append(ask)
-        
-        all_currents.append(currents)
-        all_times.append(times)
-        all_bids.append(bids)
-        all_asks.append(asks)
+            print(current, time,bid,ask)
+        ### Wait for change on page 
         sleep(random.uniform(4.5, 5))
-    
-    
-def prepare_data(currents,times,bids,asks):
-    
-    times = np.array(times)
-    bids = np.array(bids)
-    asks = np.array(asks)
-    x_axis = np.arange(1,len(bids)+1)
-    plt.clf()
-    for i in range(3):
-        plt.scatter(x_axis,bids[:,i],label = currents[0][i]+ ' BID ')
-        plt.plot(x_axis,bids[:,i])
-    plt.legend()
+        ### Change state
+        i+=1
+        if i == 1:
+            i = 0
+        ### While loop break
+        if stop == 1:
+            break
+        
+def start():
+    '''
+    Init threading and while loop to break all loops
 
-def plot_data():
+    '''
+    ### Get Tread object and start threading
+    proc = threading.Thread(target = get_data) 
+    proc.start()
+    ### Loop for break all loops 
     while True:
-        sleep(random.uniform(4.5, 5))
-        global all_currents
-        global all_times
-        global all_bids
-        global all_asks
-        prepare_data(all_currents,all_times,all_bids,all_asks)
-        
-    
-    
-x = threading.Thread(target = get_data) 
-x2 = threading.Thread(target = plot_data) 
-x.start()
-x2.start()
+        ### Init stop value
+        global stop 
+        ### Get value from user
+        stop = int(input('Pass 1 to break: '))
+        ### Break loop
+        if stop == 1:
+            break
 
-
+### Run
+if __name__ == '__main__':
+    start()
